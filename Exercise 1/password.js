d0d2bbb41b9ebb26cca6b788b95af2969a3f4f23
Subhankar Dey <s156d039@ku.edu>
2018-09-24 11:58:29 -0500

    first commit

---
+++ Exercise 1/password.js
@@ -0,0 +1,9 @@
+
+
+
+    function validateForm(){
+        var pass1 = document.getElementByName("pass1").value;
+        var pass2 = document.getElementByName("pass2").value;
+       alert("this did something!");
+
+    }
